581a160f1bc2908bf567a7e01682bf5aeb051fb9
Artem Bolotov <teeemon@gmail.com>
2023-01-17 08:48:17 +0300

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by artembolotov on 17.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
